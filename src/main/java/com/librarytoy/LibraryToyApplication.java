ddb1053e39ae7ec9bd67375ebed63a4eae3469e8
gunwooknam2023 <taeme0912@naver.com>
2024-01-24 13:13:05 +0900

    init2 commit

---
+++ src/main/java/com/librarytoy/LibraryToyApplication.java
@@ -0,0 +1,13 @@
+package com.librarytoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/librarytoy/LibraryToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarytoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
