52ae379892c368dfe901a05941ab0d1253af8545
gunwooknam2023 <taeme0912@naver.com>
2024-01-25 10:56:47 +0900

    도서 저장 정보 변경 (백엔드 + 프론트)

--- src/main/java/com/librarytoy/entity/SavedBook.java
+++ src/main/java/com/librarytoy/entity/SavedBook.java
@@ -17,41 +17,45 @@ public class SavedBook {
     private Long id;
 
     // 책 제목
-//    @Column(name = "title")
-//    private String title;
+    @Column(name = "title")
+    private String title;
 
     // 네이버 도서 정보 URL
-//    @Column(name = "link")
-//    private String link;
+    @Column(name = "link")
+    private String link;
 
     // 책 이미지 URL
-//    @Column(name = "image")
-//    private String image;
+    @Column(name = "image")
+    private String image;
 
     // 저자 이름
-//    @Column(name = "author")
-//    private String author;
+    @Column(name = "author")
+    private String author;
 
     // 판매 가격(판매 하지 않으면 비어있는 값)
-//    @Column(name = "discount")
-//    private String discount;
+    @Column(name = "discount")
+    private String discount;
 
     // 출판사
-//    @Column(name = "publisher")
-//    private String publisher;
+    @Column(name = "publisher")
+    private String publisher;
 
     // 출간일
-//    @Column(name = "pubdate")
-//    private String pubdate;
+    @Column(name = "pubdate")
+    private String pubdate;
 
     // ISBN
     @Column(name = "isbn")
     private String isbn;
 
-    // 책 설명
-//    @Column(name = "description")
-//    private String description;
     public SavedBook(BookSaveRequestDto bookSaveRequestDto){
-        this.isbn = bookSaveRequestDto.getIsbn();
+            this.title = bookSaveRequestDto.getTitle();
+            this.link = bookSaveRequestDto.getLink(); // 네이버 도서 정보 URL
+            this.image = bookSaveRequestDto.getImage(); // 책 이미지 URL
+            this.author = bookSaveRequestDto.getAuthor(); // 저자 이름
+            this.discount = bookSaveRequestDto.getDiscount(); // 판매 가격(판매 하지 않으면 비어있는 값)
+            this.publisher = bookSaveRequestDto.getPublisher(); // 출판사
+            this.pubdate = bookSaveRequestDto.getPubdate(); // 출간일
+            this.isbn = bookSaveRequestDto.getIsbn(); // ISBN
     }
 }
